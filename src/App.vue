b86823005f5d73caa74348c0a2a11478e860cea9
不坏先生git config --global user.name 不坏先生 <5474359+qazsx123@user.noreply.gitee.com>
2023-02-22 17:45:05 +0800

    表头

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -23,7 +19,7 @@
   a {
     font-weight: bold;
     color: #2c3e50;
-
+    background-color:#f6f6f6;
     &.router-link-exact-active {
       color: #42b983;
     }
