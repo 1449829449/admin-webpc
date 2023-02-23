a3d8e06fbdf7eb59fd09af8763af890a078d197a
不坏先生git config --global user.name 不坏先生 <5474359+qazsx123@user.noreply.gitee.com>
2023-02-23 17:12:58 +0800

    基础svg

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import "./libs/addelement";
+// 在main.js引入图标
+import "./icons";
 
 Vue.config.productionTip = false
 
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,44 @@
+const path = require('path')
+const WebpackBundleAnalyzer = require('webpack-bundle-analyzer')
+function resolve(dir) {
+  return path.join(__dirname, './', dir)
+}
+
 module.exports = {
- // sass 写法
- css: {
-      loaderOptions: {
-        sass: {
-          prependData: `@import '@/styles/global.scss';`,
-        },
+  publicPath: process.env.VUE_APP_ENV === 'prod' ? 'CDN' : '', // 打包路径
+  // sass 写法
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import '@/styles/global.scss';`,
       },
-  }
+    },
+  },
+  chainWebpack: (config) => {
+    if (process.env.use_analyzer) {
+      config.plugin('webpack-bundle-analyzer').use(
+        new WebpackBundleAnalyzer.BundleAnalyzerPlugin({
+          openAnalyzer: false, // 在默认浏览器中是否自动打开报告，默认 true
+        })
+      )
+    }
+    // vue.config.js svg 图标进行自动注册
+    config.module.rule('svg').exclude.add(resolve('src/icons')).end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]',
+      })
+      .end()
+      // 压缩svg图片
+      .use('svgo-loader')
+      .loader('svgo-loader')
+      .end()
+      
+  },
 }
