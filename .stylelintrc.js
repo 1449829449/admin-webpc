7d086ee517bdc5c1a31162c129b818814a6ee122
不坏先生git config --global user.name 不坏先生 <5474359+qazsx123@user.noreply.gitee.com>
2023-04-06 15:56:20 +0800

    css 格式化

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,34 +1,17 @@
 module.exports = {
-  extends: ["stylelint-config-standard", "stylelint-config-rational-order"],
-  plugins: [
-    "stylelint-order",
-    "stylelint-declaration-block-no-ignored-properties",
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
+  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
   rules: {
-    // "declaration-colon-space-after": false,
-    // "declaration-colon-space-before": false,
-    // "declaration-block-trailing-semicolon": null,
-    // "rule-empty-line-before": [
-    //   "always",
-    //   {
-    //     ignore: ["after-comment", "first-nested"],
-    //   },
-    // ],
-    // "plugin/declaration-block-no-ignored-properties": true,
-    // "selector-class-pattern": null,
-    // "no-descending-specificity": null,
-    // "selector-pseudo-element-no-unknown": [
-    //   true,
-    //   {
-    //     ignorePseudoElements: "input-placeholder",
-    //   },
-    // ],
-    // "selector-pseudo-class-no-unknown": [
-    //   true,
-    //   {
-    //     ignorePseudoClasses: ["input-placeholder"],
-    //   },
-    // ],
-    // "no-duplicate-selectors": null,
-  },
-};
+    "declaration-colon-space-after": "always-single-line",
+    "declaration-colon-space-before": "never",
+    "declaration-block-trailing-semicolon": null,
+    "rule-empty-line-before": [
+      "always",
+      {
+        ignore: ["after-comment", "first-nested"]
+      }
+    ],
+    "plugin/declaration-block-no-ignored-properties": true,
+  }
+}
+
