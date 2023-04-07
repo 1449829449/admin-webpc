d0281252bc267554f59346ce5d56326a904f7a7c
不坏先生git config --global user.name 不坏先生 <5474359+qazsx123@user.noreply.gitee.com>
2023-04-07 14:41:45 +0800

    集成husky

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
-  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
+  extends: ["stylelint-config-standard", "stylelint-config-rational-order"],
+  plugins: [
+    "stylelint-order",
+    "stylelint-declaration-block-no-ignored-properties",
+  ],
   rules: {
     "declaration-colon-space-after": "always-single-line",
     "declaration-colon-space-before": "never",
@@ -8,10 +11,9 @@ module.exports = {
     "rule-empty-line-before": [
       "always",
       {
-        ignore: ["after-comment", "first-nested"]
-      }
+        ignore: ["after-comment", "first-nested"],
+      },
     ],
     "plugin/declaration-block-no-ignored-properties": true,
-  }
-}
-
+  },
+};
