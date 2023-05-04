7610d33eff10a7db88690f150b8e4c314a250037
不坏先生git config --global user.name 不坏先生 <5474359+qazsx123@user.noreply.gitee.com>
2023-05-04 15:21:07 +0800

    feat: 撒二十大萨达是
    undefined

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,32 +5,32 @@
     <el-button type="success">成功按钮</el-button>
     <el-button type="info">信息按钮</el-button>
     <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
+    <el-button type="danger">危险按钮1</el-button>
     <svg-icon icon-class="jinxingzhong"></svg-icon>
     <div class="login-btn" v-permission="true">6666</div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    {{ 100 | gold}}
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{ 100 | gold }}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-import { login } from '../api/login'
+import { login } from "../api/login";
 
 export default {
-  name: 'Home',
+  name: "Home",
   // components: {
   //   HelloWorld
   // }
   async mounted() {
-    console.log(this.$store.state.modulA.name)
+    console.log(this.$store.state.modulA.name);
     try {
-      let a = await login()
-      console.log(a)
+      let a = await login();
+      console.log(a);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   },
-}
+};
 </script>
